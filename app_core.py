20e957c434652e71df0610a5d894a6e570413ffd
asanoshin <litpenguin730822@gmail.com>
2023-12-08 14:41:25 +0800

    表單

--- app_core.py
+++ app_core.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +22,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    phone = request.form['phone']
+    option = request.form['option']
+    print(f"{phone}, {option}")
+    return f"提交成功！電話：{phone}, 選項：{option}"
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
