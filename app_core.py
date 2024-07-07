13c0f75511ea6a24608c3efa54e89102a87b2449
asanoshin <litpenguin730822@gmail.com>
2024-07-07 14:33:49 +0800

    updae

--- app_core.py
+++ app_core.py
@@ -24,7 +24,9 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+    print("-----------------------------body-----------------------------")
+    print(body)
+    print("---------------------------body end test5---------------------------")
     # handle webhook body
     try:
         handler.handle(body, signature)
