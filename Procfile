75eac00afd1ac3b6e162661c4452626671fbcbb6
asanoshin <litpenguin730822@gmail.com>
2024-07-23 13:19:19 +0800

    update

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn app:app
-worker: celery -A app.celery worker --loglevel=info
+web: gunicorn run_task:app
+worker: celery -A tasks worker --loglevel=info
--- tasks.py
+++ tasks.py
@@ -1,12 +1,12 @@
+
+import os
 from celery import Celery
 
-# 創建Celery應用
-app = Celery('tasks', broker='rediss://:p2747f965299f0534af9dd1f1c3fbb57cc9ef45a3bdaac6dcf70da1ca1dc45daa@ec2-54-91-164-149.compute-1.amazonaws.com:20070?ssl_cert_reqs=CERT_NONE', backend='rediss://:p2747f965299f0534af9dd1f1c3fbb57cc9ef45a3bdaac6dcf70da1ca1dc45daa@ec2-54-91-164-149.compute-1.amazonaws.com:20070?ssl_cert_reqs=CERT_NONE')
+redis_url = os.environ.get('REDIS_URL', 'rediss://p2747f965299f0534af9dd1f1c3fbb57cc9ef45a3bdaac6dcf70da1ca1dc45daa@ec2-54-91-164-149.compute-1.amazonaws.com:20070')
+app = Celery('tasks', broker=redis_url, backend=redis_url)
 
-# 定義一個耗時的任務
 @app.task
 def long_running_task(x, y):
-    # 模擬一個耗時的操作
     import time
     time.sleep(5)
     return x + y
