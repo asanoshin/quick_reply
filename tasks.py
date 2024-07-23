90d361898d3876524e5faad400dab40b2a29d9c6
asanoshin <litpenguin730822@gmail.com>
2024-07-23 13:27:02 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,8 +1,7 @@
-
 import os
 from celery import Celery
 
-redis_url = os.environ.get('REDIS_URL', 'rediss://:p2747f965299f0534af9dd1f1c3fbb57cc9ef45a3bdaac6dcf70da1ca1dc45daa@ec2-54-91-164-149.compute-1.amazonaws.com:20070?ssl_cert_reqs=CERT_NONE', backend='rediss://:p2747f965299f0534af9dd1f1c3fbb57cc9ef45a3bdaac6dcf70da1ca1dc45daa@ec2-54-91-164-149.compute-1.amazonaws.com:20070?ssl_cert_reqs=CERT_NONE')
+redis_url = os.environ.get('REDIS_URL', 'rediss://:p2747f965299f0534af9dd1f1c3fbb57cc9ef45a3bdaac6dcf70da1ca1dc45daa@ec2-54-91-164-149.compute-1.amazonaws.com:20070?ssl_cert_reqs=CERT_NONE')
 app = Celery('tasks', broker=redis_url, backend=redis_url)
 
 @app.task
