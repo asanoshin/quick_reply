d416eaf864df366eefa537c5b626546c06ca309e
asanoshin <litpenguin730822@gmail.com>
2024-07-23 13:31:09 +0800

    Update run_task.py

--- run_task.py
+++ run_task.py
@@ -10,4 +10,4 @@ def hello():
     return f"Task submitted with id {result.id}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
