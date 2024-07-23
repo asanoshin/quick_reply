aa4746dac87f2a47e9bb1816c2763a615fb38206
asanoshin <litpenguin730822@gmail.com>
2024-07-24 06:27:03 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import time
+import ssl
 
 # 假設您的 Redis URL
 redis_url = "rediss://:pac2484ffa244b6dbed12e3e37b44090ce54d8e6685ac09a89825acd0980d4081@ec2-3-218-138-147.compute-1.amazonaws.com:17669"
@@ -10,11 +11,12 @@ app = Celery('tasks', broker=redis_url, backend=redis_url)
 # 配置 Celery
 app.conf.update(
     broker_use_ssl={
-        'ssl_cert_reqs': None  # 禁用證書驗證
+        'ssl_cert_reqs': ssl.CERT_NONE  # 使用 ssl.CERT_NONE 而不是 None
     },
     redis_backend_use_ssl={
-        'ssl_cert_reqs': None  # 禁用證書驗證
-    }
+        'ssl_cert_reqs': ssl.CERT_NONE  # 使用 ssl.CERT_NONE 而不是 None
+    },
+    broker_connection_retry_on_startup=True
 )
 
 @app.task
